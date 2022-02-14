a9a003d5b979c1432eca6c7450182affe8943c35
Jonny L Greenwood <jonnylgreenwood@gmail.com>
2022-02-14 23:19:16 +0000

    Added two methods to get adjacent cols

--- minesweeper.py
+++ minesweeper.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np
 
 class GridCell:
     def __init__(self, name, repr, is_mine, is_empty, is_flag) -> None:
@@ -36,8 +37,11 @@ class Grid:
         display_rows = [' '.join(map(str, i)) for i in self.grid]
         return '\n'.join(map(str, display_rows))
 
+    def __str__(self) -> str:
+        return self.__repr__()
+
     def __repr__(self) -> str:
-        return self.display_grid()
+        return f'grid: {self.rows} x {self.cols}\nmines: {self.mine_count}\n{self.display_grid()}'
 
     def add_mines(self):
         mines_planted = 0
@@ -52,9 +56,27 @@ class Grid:
 
     def get_adj_mines(self, coord):
         row, col = coord
-        adj_cells = [i for i in range(-1, 2, 1)]
-        adj_grid = [(i, adj_cells) for i in range(-1, 2, 1)]
-        return adj_grid
+        adjacent = []
+        mine_count = 0
+        for col_offset in range(-1, 2, 1):
+            col_final = col+col_offset
+            if col_final > 0 and col_final < self.cols:
+                for row_offset in range(-1, 2, 1):
+                    row_final = row+row_offset
+                    if row_final > 0 and row_final < self.rows:
+                        adjacent.append(self.grid[row_final][col_final])
+                        if self.grid[row_final][col_final].is_mine:
+                            mine_count += 1
+        return mine_count
+
+    def get_adj_mines2(self, coord):
+        offset = np.array([-1, 0 , 1])
+        row, col = coord
+        rows = row + offset
+        cols = col + offset
+        valid_rows = offset[(offset>=0) & (offset<=self.rows)]        
+        valid_cols = offset[(offset>=0) & (offset<=self.cols)]
+
     
         # multi line implementation of create grid - not used
     def create_grid_legacy(self):
@@ -72,4 +94,4 @@ result.add_mines()
 
 print(result)
 
-print(result.get_adj_mines((2,3)))
+print(result.get_adj_mines2((2,2)))
