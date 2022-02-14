fcfb4035116fa31032ef13fe8d33c0ec139625e7
Jonny Greenwood SP <jonnylgreenwood@gmail.com>
2022-02-14 22:14:05 +0000

    Added displaygrid functionality Started get_adj_mines

--- minesweeper.py
+++ minesweeper.py
@@ -32,14 +32,29 @@ class Grid:
         grid = [[self.empty_cell for _ in range(self.rows)] for _ in range(self.cols)]
         return grid
 
+    def display_grid(self):
+        display_rows = [' '.join(map(str, i)) for i in self.grid]
+        return '\n'.join(map(str, display_rows))
+
+    def __repr__(self) -> str:
+        return self.display_grid()
+
     def add_mines(self):
         mines_planted = 0
         grid = self.grid
-        row = random.randint(0, self.rows-1)
-        col = random.randint(0, self.cols-1)
-        grid[row][col] = self.mine_cell
+        while mines_planted <= self.mine_count:
+            row = random.randint(0, self.rows-1)
+            col = random.randint(0, self.cols-1)
+            if grid[row][col] != self.mine_cell:
+                grid[row][col] = self.mine_cell
+                mines_planted += 1
         return result
 
+    def get_adj_mines(self, coord):
+        row, col = coord
+        adj_cells = [i for i in range(-1, 2, 1)]
+        adj_grid = [(i, adj_cells) for i in range(-1, 2, 1)]
+        return adj_grid
     
         # multi line implementation of create grid - not used
     def create_grid_legacy(self):
@@ -55,4 +70,6 @@ result = Grid(10, 10, 10)
 
 result.add_mines()
 
-print(result.grid)
+print(result)
+
+print(result.get_adj_mines((2,3)))
